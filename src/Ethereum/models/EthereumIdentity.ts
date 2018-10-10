69fa9931f474cd6c08f74d826a602e26e4d2f5d1
Eric Maino <ericmai@microsoft.com>
2018-10-10 13:48:38 -0700

    Adding test to verify EthereumNetworkId

--- src/Ethereum/models/EthereumIdentity.ts
+++ src/Ethereum/models/EthereumIdentity.ts
@@ -1,4 +1,4 @@
-import pad = require('pad');
+import { format } from 'url';
 
 export class EthereumIdentity {
     private networkId: number;
@@ -8,10 +8,20 @@ export class EthereumIdentity {
     constructor(networkId: number, originHash: string) {
         this.networkId = networkId ? networkId : 0;
         this.originHash = originHash;
-        this.identity = `${pad(this.networkId.toString(), 10, "0")}-${originHash.substr(2, 10)}`;
+        this.identity = `${this.PadLeft(this.networkId.toString(), 10, "0")}-${originHash.substr(2, 10)}`;
     }
 
     public AsString(): string {
         return this.identity;
+    }
+
+    private PadLeft(input: string, size: number, char: string): string {
+        let result = input;
+        if (input.length < size) {
+            const padSize = size - input.length;
+            result = char.repeat(padSize).slice(0, padSize) + input;
+        }
+
+        return result;
     }
 }
