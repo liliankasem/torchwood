f1b822b131a9187b8f953b0cf12133a0393634a4
Lilian Kasem <likasem@microsoft.com>
2018-10-10 10:27:47 -0700

    fixing package and pad()

--- src/Ethereum/models/EthereumIdentity.ts
+++ src/Ethereum/models/EthereumIdentity.ts
@@ -8,7 +8,7 @@ export class EthereumIdentity {
     constructor(networkId: number, originHash: string) {
         this.networkId = networkId ? networkId : 0;
         this.originHash = originHash;
-        this.identity = `${pad(10, this.networkId, "0")}-${originHash.substr(2, 10)}`;
+        this.identity = `${pad(this.networkId.toString(), 10, "0")}-${originHash.substr(2, 10)}`;
     }
 
     public AsString(): string {
